40c93d43c8c4980d232267cf85bbeaf17585f1aa
steveolson111 <steveolsonbyunc@gmail.com>
2025-09-08 11:39:03 -0600

    Added the 3 files from the Bike Share to the folder

--- BikeShare.R
+++ BikeShare.R
@@ -1,3 +1,14 @@
 library(tidyverse)
-
 library(tidymodels)
+library(vroom)
+
+#vroom()
+#dplyr::glimpse(dataset) - lists the variable type of each column
+#skimr::skim(dataset) - nice overview of the dataset
+#DataExplorer::plot_intro(dataset) - visualization of glimpse()
+#DataExplorer::plot_correlation(dataset) - correlation heat map between variables
+#DataExplorer::plot_bar(dataset) - bar charts of all discrete variables
+#DataExplorer::plot_histrograms(dataset) - histograms of all numerical variables
+#DataExplorer::plot_missing(dataset) - percent missing in each column
+#GGally::ggpairs(dataset) - 1/2 scatterplot and 1/2 correlation heat map
+
