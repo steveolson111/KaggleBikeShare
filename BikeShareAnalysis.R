8233c479b9f7cd9b773112cc28fb1c0265454c2f
steveolson111 <steveolsonbyunc@gmail.com>
2025-09-11 20:44:08 -0600

    Finished Kaggle Bikeshare Homework!

--- BikeShareAnalysis.R
+++ BikeShareAnalysis.R
@@ -7,11 +7,11 @@ library(GGally)
 train <- vroom("C:/Users/Administrator/OneDrive - Brigham Young University/1School/Stat 348/BikeShare/train.csv")
 test <- vroom("C:/Users/Administrator/OneDrive - Brigham Young University/1School/Stat 348/BikeShare/test.csv")
 glimpse(train)
-#ggpairs(train)
+#ggpairs(train) # Creates awesome correlation graphs for EDA!
 
 #Recognize something about weather...
 
-
+#4 Plot HW
 library(patchwork)
 plot1 <- ggplot(data=train, mapping=aes(x=registered, y=count)) + 
   geom_point()+ geom_smooth(se=FALSE)+ labs(title="Bike Usage 
@@ -34,16 +34,20 @@ plot4 <- ggplot(data=train, mapping=aes(x=atemp, y=count)) +
 
 (plot1 + plot2) / (plot3 + plot4) #4 panel plot
 
-library(tidymodels)
-train <- train %>% select(-casual, -registered)
+# Logistic Regression analysis to get slightly more accurate than just linear
+train <- train %>% select(-casual, -registered) %>%
+  mutate(log_count = log1p(count)) %>% 
+  select(-count)
 ## Setup and Fit the Linear Regression Model
 my_linear_model <- linear_reg() %>% #Type of model
   set_engine("lm") %>% # Engine = What R function to use
-  set_mode("regression") %>% # Regression just means quantitative response6
-  fit(formula=count~., data=train) 
+  set_mode("regression") %>% # Regression just means quantitative response
+  fit(formula=log_count~., data=train) 
 ## Generate Predictions Using Linear Model
 bike_predictions <- predict(my_linear_model,
-                            new_data=test) # Use fit to predict
+                            new_data=test)# Use fit to predict
+bike_predictions <- bike_predictions %>%
+  mutate(.pred = expm1(.pred))
 bike_predictions ## Look at the output
 
 ## Format the Predictions for Submission to Kaggle
@@ -53,5 +57,5 @@ bind_cols(., test) %>% #Bind predictions with test data
   rename(count=.pred) %>% #rename pred to count (for submission to Kaggle)
   mutate(count=pmax(0, count)) %>% #pointwise max of (0, prediction)6
   mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
-## Write out the file9
+## Write out the file
 vroom_write(x=kaggle_submission, file="./LinearPreds.csv", delim=",")
