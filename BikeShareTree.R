88b55246e3b5b3a8ac0c1c326963c1a86d929082
steveolson111 <steveolsonbyunc@gmail.com>
2025-09-19 11:51:45 -0600

    Tree first try!

--- BikeShareTree.R
+++ BikeShareTree.R
@@ -1,51 +1,23 @@
-library(tidyverse)
+install.packages("rpart")
 library(tidymodels)
+library(tidyverse)
 library(vroom)
-#install.packages('GGally')
-library(GGally)
-library(lubridate)
+library(rpart)
 
 train <- vroom("C:/Users/Administrator/OneDrive - Brigham Young University/1School/Stat 348/BikeShare/train.csv")
 test <- vroom("C:/Users/Administrator/OneDrive - Brigham Young University/1School/Stat 348/BikeShare/test.csv")
 glimpse(train)
-#ggpairs(train) # Creates awesome correlation graphs for EDA!
 
-#Recognize something about weather...
-
-#4 Plot HW
-library(patchwork)
-plot1 <- ggplot(data=train, mapping=aes(x=registered, y=count)) + 
-  geom_point()+ geom_smooth(se=FALSE)+ labs(title="Bike Usage 
-  & Registered Users",
-                                            x="Number of Registered Users", y="Total Bike Count")
-plot2 <- ggplot(data=train, mapping=aes(x = factor(weather,
-                                                   levels = c(4, 3, 2, 1), labels = c("Heavy Precip", "Light 
-  Precip", "Cloudy", "Clear" )), y=count)) +
-  geom_bar(stat="identity", fill="steelblue") + 
-  labs(title = "Bike Usage by Weather Type", x="Weather Type",
-       y="Total Bike Count")
-plot2
-plot3 <- ggplot(data=train, mapping=aes(x = factor(workingday,
-                                                   levels = c(0, 1), labels = c("No", "Yes")), y=count)) + 
-  geom_bar(stat="identity", fill="steelblue")+ labs(title="Working Day Usage",
-                                                    x= "Working Day", y="Total Bike Count")
-plot4 <- ggplot(data=train, mapping=aes(x=atemp, y=count)) + 
-  geom_point()+ geom_smooth(se=FALSE)+ labs(title="Bike Usage based on 'Feels-like' Temperature",
-                                            x="'Feels-like' Temperature (C)", y="Total Bike Count")
-
-#(plot1 + plot2) / (plot3 + plot4) #4 panel plot
-
-# Logistic Regression analysis to get slightly more accurate than just linear
 train <- train %>% select(-casual, -registered) %>%
   mutate(log_count = log1p(count)) %>% 
   select(-count)
 
 train <- train %>% mutate(dow = wday(datetime, week_start = 1) - 1,  # 0 = Monday
-    hour = hour(datetime),
-    hour_of_week = dow * 24 + hour)
+                          hour = hour(datetime),
+                          hour_of_week = dow * 24 + hour)
 test <- test %>% mutate( dow = lubridate::wday(datetime, week_start = 1) - 1,
-    hour = lubridate::hour(datetime),
-    hour_of_week = dow * 24 + hour)
+                         hour = lubridate::hour(datetime),
+                         hour_of_week = dow * 24 + hour)
 train <- train %>%
   mutate(hour_category = case_when(
     hour >= 6 & hour < 10 ~ "morning_commute",
@@ -61,9 +33,7 @@ test <- test %>%
     hour >= 16 & hour < 20 ~ "evening_commute",
     TRUE ~ "night"))
 
-
-## Workflows Homework
-library(tidymodels)
+## Create a workflow with recipe & model
 bike_recipe <- recipe(log_count~., data=train) %>% # Set model formula and dataset
   step_mutate(weather=ifelse(weather==4, 3, weather)) %>% #Mutate for just 3 categories
   step_mutate(weather=factor(weather, levels= c(1,2,3), labels=c("Clear", "Cloudy", "Severe"))) %>% #Make something a factor
@@ -72,10 +42,9 @@ bike_recipe <- recipe(log_count~., data=train) %>% # Set model formula and datas
   step_date(datetime, features="dow") %>% # gets day of week and month and year
   step_time(datetime, features=c("hour", "minute")) %>% #create time variable
   step_mutate(hour_of_week_sin = sin(2 * pi * hour_of_week / 168),
-             hour_of_week_cos = cos(2 * pi * hour_of_week / 168))%>%
+              hour_of_week_cos = cos(2 * pi * hour_of_week / 168))%>%
   step_mutate(hour_sin = sin(2 * pi * hour / 168),
               hour_cos = cos(2 * pi * hour / 168))%>%
-
   step_rm(datetime)%>%
   step_dummy(all_nominal_predictors()) %>% #create dummy variables
   step_zv(all_predictors()) %>% #removes zero-variance predictors
@@ -84,28 +53,52 @@ bike_recipe <- recipe(log_count~., data=train) %>% # Set model formula and datas
 prepped_recipe <- prep(bike_recipe) # Sets up the preprocessing using myDataSet
 baked_dataset <-bake(prepped_recipe, new_data=test)
 
-## Setup and Fit the Linear Regression Model
-my_linear_model <- linear_reg() %>% #Type of model
-set_engine("lm") %>% # Engine = What R function to use
-set_mode("regression") # Regression just means quantitative response
-## Generate Predictions Using Linear Model
-#bike_predictions <- predict(my_linear_model,
-#new_data=test)# Use fit to predict
+my_mod <- decision_tree(tree_depth = tune(),
+                        cost_complexity = tune(),
+                        min_n=tune()) %>% #Type of model
+  set_engine("rpart") %>% # What R function to use
+  set_mode("regression")
 
 ## Combine into a Workflow and fit
 bike_workflow <- workflow() %>%
   add_recipe(bike_recipe) %>%
-  add_model(my_linear_model) %>%
-  fit(data=train)
-#fit(formula=log_count~., data=train) 
+  add_model(my_mod)
 
-## Run all the steps on test data
-lin_preds <- predict(bike_workflow, new_data = test)
 
-bike_predictions <- lin_preds %>%
+## Set up grid of tuning values for decision tree
+tree_grid <- grid_regular(
+  tree_depth(range = c(2, 15)),
+  cost_complexity(range = c(-5, -1)),  # log10 scale: 10^-5 to 10^-1
+  min_n(range = c(2, 20)),
+  levels = 5 # how many values to try per parameter
+)
+## Set up K-fold CV
+# Tune the workflow
+set.seed(123)
+folds <- vfold_cv(train, v = 5)
+
+tuned_results <- tune_grid(
+  bike_workflow,
+  resamples = folds,
+  grid = tree_grid,
+  metrics = metric_set(rmse, rsq))
+
+## Find best tuning parameters
+best_params <- select_best(tuned_results, "rmse")
+#best_params
+
+## Finalize workflow and predict
+final_wf <- finalize_workflow(
+  bike_workflow,
+  best_params)
+
+final_model <- fit(final_wf, data = train)
+
+tree_preds <- predict(final_model, new_data = test)
+## Finalize workflow and predict
+bike_predictions <- tree_preds %>%
   mutate(.pred = expm1(.pred))
 bike_predictions ## Look at the output
-
 
 ## Format the Predictions for Submission to Kaggle
 kaggle_submission <- bike_predictions %>%
