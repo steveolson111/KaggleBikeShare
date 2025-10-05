b5296a83b6592e6b0b14f6cdcdff96c454aba7ad
steveolson111 <steveolsonbyunc@gmail.com>
2025-10-04 22:54:36 -0600

    I achieved my goal!
    My personal goal was to get under .40 and I did!

--- BikeShareBoost&Bart.R
+++ BikeShareBoost&Bart.R
@@ -5,7 +5,6 @@ library(vroom)
 library(rpart)
 library(bonsai)
 library(lightgbm)
-library(GGally)
 
 
 ## CV tune, finalize and predict here and save results
@@ -18,60 +17,36 @@ train <- train %>% select(-casual, -registered) %>%
   mutate(log_count = log1p(count)) %>% 
   select(-count)
 
-train <- train %>% mutate(dow = wday(datetime, week_start = 1) - 1,  # 0 = Monday
-                          hour = hour(datetime),
-                          hour_of_week = dow * 24 + hour)
-test <- test %>% mutate( dow = lubridate::wday(datetime, week_start = 1) - 1,
-                         hour = lubridate::hour(datetime),
-                         hour_of_week = dow * 24 + hour)
-train <- train %>%
-  mutate(hour_category = case_when(
-    hour >= 6 & hour < 10 ~ "morning_commute",
-    hour >= 10 & hour < 16 ~ "midday",
-    hour >= 16 & hour < 20 ~ "evening_commute",
-    TRUE ~ "night"
-  ))
-
-test <- test %>%
-  mutate(hour_category = case_when(
-    hour >= 6 & hour < 10 ~ "morning_commute",
-    hour >= 10 & hour < 16 ~ "midday",
-    hour >= 16 & hour < 20 ~ "evening_commute",
-    TRUE ~ "night"))
 
 ## Create a workflow with recipe & model
 bike_recipe <- recipe(log_count~., data=train) %>% # Set model formula and dataset
   step_mutate(weather=ifelse(weather==4, 3, weather)) %>% #Mutate for just 3 categories
   step_mutate(weather=factor(weather, levels= c(1,2,3), labels=c("Clear", "Cloudy", "Severe"))) %>% #Make something a factor
   step_mutate(season=factor(season, levels= c(1,2,3,4), labels=c("Spring", "Summer", "Fall", "Winter"))) %>% #Make something a factor
-  step_mutate(newTemp=temp*atemp, difTemp=temp-atemp) %>% #Create 3 new variables
-  step_date(datetime, features="dow") %>% # gets day of week and month and year
+  step_mutate(wind_temp = windspeed * temp)%>%
+  #step_date(datetime, features="dow") %>% # gets day of week and month and year
+  step_date(datetime, features = c("year", "month", "dow"))%>%
   step_time(datetime, features=c("hour", "minute")) %>% #create time variable
-  step_mutate(hour_of_week_sin = sin(2 * pi * hour_of_week / 168),
-              hour_of_week_cos = cos(2 * pi * hour_of_week / 168))%>%
-  #step_mutate(hour_sin = sin(2 * pi * hour / 24),   #best result was with /168 here and line below?
-              #hour_cos = cos(2 * pi * hour / 24))%>%
-  step_mutate(hour = as.factor(hour))%>%
+  step_mutate(hour_of_week = as.numeric(datetime_dow) * 24 + datetime_hour)%>%
+  step_mutate(
+    hour_of_week_sin = sin(2 * pi * hour_of_week / 168),
+    hour_of_week_cos = cos(2 * pi * hour_of_week / 168)) %>%
+  step_mutate(
+    hour_category = case_when(
+      datetime_hour >= 6 & datetime_hour < 9 ~ "morning_commute",
+      datetime_hour >= 9 & datetime_hour < 12 ~ "midmorn",
+      datetime_hour >= 12 & datetime_hour < 16 ~ "afternoon",
+      datetime_hour >= 16 & datetime_hour < 18 ~ "evening_commute",
+      TRUE ~ "night" ) )%>%
+  step_mutate(hour_category = factor(hour_category))%>%
+  step_mutate(hour_cat_interaction = interaction(as.factor(datetime_hour), hour_category, sep = "_")) %>%
   step_rm(datetime)%>%
   step_dummy(all_nominal_predictors()) %>% #create dummy variables
   step_zv(all_predictors()) %>% #removes zero-variance predictors
-  step_normalize(temp, atemp, humidity, windspeed)%>%
+  step_normalize(atemp, humidity, windspeed)%>%
   step_corr(all_numeric_predictors(), threshold=0.8) # removes > than .8 corr
 prepped_recipe <- prep(bike_recipe) # Sets up the preprocessing using myDataSet
-baked_dataset <-bake(prepped_recipe, new_data=test)
-
-########Checking what was correlated... 
-#tidy(prepped_recipe, number = 14)
-# now checking what it all correlated with...
-#ggpairs(baked_dataset)
-
-
-
-#boost_model <- boost_tree(tree_depth=tune(),
-                          #trees=tune(),
-                          #learn_rate=tune()) %>%
-  #set_engine("lightgbm") %>% #or "xgboost" but lightgbm is faster
-  #set_mode("regression")
+baked_dataset <-bake(prepped_recipe, new_data=train)
 
 bart_model <- bart(trees=tune()) %>% # BART figures out depth and learn_rate
   set_engine("dbarts") %>% # might need to install
@@ -86,7 +61,7 @@ set.seed(123)
 ## Set up grid of tuning values for decision tree
 bart_grid <- grid_regular(
   trees(range = c(50, 500)),
- levels = 5)
+  levels = 1)
 
 ## Set up K-fold CV
 # Tune the workflow
@@ -107,7 +82,9 @@ final_wf <- finalize_workflow(
   bike_workflow,
   best_params)
 
-final_model <- fit(final_wf, data = train)
+final_model <- fit(final_wf, data = train) 
+
+################################################################################
 
 tree_preds <- predict(final_model, new_data = test)
 ## Finalize workflow and predict
@@ -124,3 +101,40 @@ kaggle_submission <- bike_predictions %>%
   mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
 ## Write out the file
 vroom_write(x=kaggle_submission, file="./LinearPreds.csv", delim=",")
+
+##########################################################################
+##########################################################################
+##########################################################################
+#Try to guess what features were most helpful?
+
+
+#To help see the column names!
+processed_train <- juice(prepped_recipe)
+colnames(processed_train)
+
+# Extract the dbarts model object
+bart_fit <- extract_fit_engine(final_model)
+
+# Get variable importance scores (number of times variable used in splits)
+importance_scores <- bart_fit$varcount %>%
+  colSums() %>%
+  sort(decreasing = TRUE)
+
+# Turn into a tibble for plotting
+importance_df <- tibble(
+  variable = names(importance_scores),
+  importance = importance_scores
+)
+
+# Plot top 20 variables
+library(ggplot2)
+
+importance_df %>%
+  slice_max(order_by = importance, n = 20) %>%
+  ggplot(aes(x = reorder(variable, importance), y = importance)) +
+  geom_col(fill = "steelblue") +
+  coord_flip() +
+  labs(title = "BART Variable Importance",
+       x = "Variable",
+       y = "Importance (Split Count)") +
+  theme_minimal()
--- BikeShareFinalUnder40.R
+++ BikeShareFinalUnder40.R
@@ -17,37 +17,6 @@ train <- train %>% select(-casual, -registered) %>%
   mutate(log_count = log1p(count)) %>% 
   select(-count)
 
-train <- train %>% mutate(dow = wday(datetime, week_start = 1) - 1,  # 0 = Monday
-                          hour = hour(datetime),
-                          hour_of_week = dow * 24 + hour)
-test <- test %>% mutate( dow = lubridate::wday(datetime, week_start = 1) - 1,
-                         hour = lubridate::hour(datetime),
-                         hour_of_week = dow * 24 + hour)
-
-train <- train %>%
-  mutate(hour_category = case_when(
-    hour >= 6 & hour < 10 ~ "morning_commute",
-    hour >= 10 & hour < 16 ~ "midday",
-    hour >= 16 & hour < 20 ~ "evening_commute",
-    TRUE ~ "night" ))
-
-test <- test %>%
-  mutate(hour_category = case_when(
-    hour >= 6 & hour < 10 ~ "morning_commute",
-    hour >= 10 & hour < 16 ~ "midday",
-    hour >= 16 & hour < 20 ~ "evening_commute",
-    TRUE ~ "night"))
-
-train <- train %>%
-  mutate(too_cold = if_else(temp < 2, 1, 0),
-    ideal_temp = if_else(temp >= 2 & temp <= 33, 1, 0),
-    too_hot = if_else(temp > 33, 1, 0) )
-
-test <- test %>%
-  mutate(too_cold = if_else(temp < 2, 1, 0),
-    ideal_temp = if_else(temp >= 2 & temp <= 33, 1, 0),
-    too_hot = if_else(temp > 33, 1, 0))
-
 
 ## Create a workflow with recipe & model
 bike_recipe <- recipe(log_count~., data=train) %>% # Set model formula and dataset
@@ -55,40 +24,29 @@ bike_recipe <- recipe(log_count~., data=train) %>% # Set model formula and datas
   step_mutate(weather=factor(weather, levels= c(1,2,3), labels=c("Clear", "Cloudy", "Severe"))) %>% #Make something a factor
   step_mutate(season=factor(season, levels= c(1,2,3,4), labels=c("Spring", "Summer", "Fall", "Winter"))) %>% #Make something a factor
   step_mutate(wind_temp = windspeed * temp)%>%
-  step_poly(humidity, degree = 2)%>%
-  #step_mutate(dow_sin = sin(2 * pi * dow / 7),
-   #           dow_cos = cos(2 * pi * dow / 7))%>%
-  #step_poly(temp, degree = 2)%>%
-  step_date(datetime, features="dow") %>% # gets day of week and month and year
+  #step_date(datetime, features="dow") %>% # gets day of week and month and year
+  step_date(datetime, features = c("year", "month", "dow"))%>%
   step_time(datetime, features=c("hour", "minute")) %>% #create time variable
-  step_mutate(is_rush_hour = as.factor(if_else(hour %in% c(7,8,9,16,17,18), 1, 0)))%>%
-  step_mutate(is_work_hour = as.factor(if_else(hour >= 9 & hour < 17, 1, 0)) )%>%
-  step_mutate(is_daylight = as.factor(if_else(hour >= 7 & hour <= 19, 1, 0)) )%>%
-  step_mutate(hour_of_week_sin = sin(2 * pi * hour_of_week / 168),
-              hour_of_week_cos = cos(2 * pi * hour_of_week / 168))%>%
-  step_mutate(hour_cat_interaction = interaction(hour, hour_category, sep = "_"))%>%
-  #step_mutate(hour_sin = sin(2 * pi * hour / 24),
-              #hour_cos = cos(2 * pi * hour / 24))%>%
-  step_mutate(month = as.factor(lubridate::month(datetime)))%>%
-  step_mutate(hour_sq = hour^2)%>%
+step_mutate(hour_of_week = as.numeric(datetime_dow) * 24 + datetime_hour)%>%
   step_mutate(
-    is_morning = as.factor(if_else(hour >= 6 & hour < 12, 1, 0)),
-    is_evening = as.factor(if_else(hour >= 17 & hour < 21, 1, 0)),
-    is_holiday_weekend = as.factor(if_else(holiday == 1 & dow %in% c(0,6), 1, 0)) )%>%
-  #step_mutate(hour = as.factor(hour))%>%
+    hour_of_week_sin = sin(2 * pi * hour_of_week / 168),
+    hour_of_week_cos = cos(2 * pi * hour_of_week / 168)) %>%
+  step_mutate(
+    hour_category = case_when(
+      datetime_hour >= 6 & datetime_hour < 9 ~ "morning_commute",
+      datetime_hour >= 9 & datetime_hour < 11 ~ "midmorn",
+      datetime_hour >= 11 & datetime_hour < 16 ~ "afternoon",
+      datetime_hour >= 16 & datetime_hour < 18 ~ "evening_commute",
+      TRUE ~ "night" ) )%>%
+  step_mutate(hour_category = factor(hour_category))%>%
+  step_mutate(hour_cat_interaction = interaction(as.factor(datetime_hour), hour_category, sep = "_")) %>%
   step_rm(datetime)%>%
   step_dummy(all_nominal_predictors()) %>% #create dummy variables
   step_zv(all_predictors()) %>% #removes zero-variance predictors
-  step_normalize(atemp, windspeed)%>%
+  step_normalize(atemp, humidity, windspeed)%>%
   step_corr(all_numeric_predictors(), threshold=0.8) # removes > than .8 corr
 prepped_recipe <- prep(bike_recipe) # Sets up the preprocessing using myDataSet
 baked_dataset <-bake(prepped_recipe, new_data=train)
-
-#boost_model <- boost_tree(tree_depth=tune(),
-#trees=tune(),
-#learn_rate=tune()) %>%
-#set_engine("lightgbm") %>% #or "xgboost" but lightgbm is faster
-#set_mode("regression")
 
 bart_model <- bart(trees=tune()) %>% # BART figures out depth and learn_rate
   set_engine("dbarts") %>% # might need to install
@@ -180,4 +138,3 @@ importance_df %>%
        x = "Variable",
        y = "Importance (Split Count)") +
   theme_minimal()
- 
